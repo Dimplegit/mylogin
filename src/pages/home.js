71426d0c3cff20476fcb6238d69ffaaffa26738d
Dimple <d.dhiman1715@gmail.com>
2022-07-22 10:27:07 +0530

    dimple

---
+++ src/pages/home.js
@@ -0,0 +1,48 @@
+
+import React from "react"
+class home extends React.Component{
+
+    render(){
+    
+            
+
+
+<div className="container headers">
+        <div className="row justify-content-md-center">
+            <div className="col col-lg-6">
+                <img src="img/logo.jpg" className="rounded mx-auto d-block" alt="..."></img>
+            </div>
+        </div>
+ 
+        <div className="row justify-content-md-center align-items-center ">
+            <div className="col-lg-4 col-sm-12 mb-4 ">
+                <a href="pub.html">
+                    <div className="box1 mx-auto" style="width:200px;height:200px">
+                        <img className="pic-1" src="img/icons/search.png"></img>
+                        <h3 className="title">Search</h3>
+                    </div>
+                </a>
+            </div>
+            <div className="col-lg-4 col-sm-12 mb-4 ">
+                <a href="adv.html">
+                    <div className="box1 mx-auto" style="width:200px;height:200px">
+                        <img className="pic-1" src="img/icons/adv.jpg"></img>
+                        <h3 className="title">Login/Register</h3>
+                    </div>
+                </a>
+            </div>
+
+
+            <p><b>Note:</b> If your Bar Association is not showing contact to below information
+                <br>
+                <b>Ms. Monika Jangra </b></br>
+                <br> ( Executive Committee Member || High Court,Chandigarh )</br>
+                <br>info@eadv.in | App Ver. 1.0.1.0</br>
+            </p>
+        </div>
+
+    </div>
+
+    
+
+export default home;
